3134050995f4cde285ed5b0859efcb5a118ef2ba
feiyangmeiyu <806567171@qq.com>
2020-11-01 15:49:35 +0100

    add db

--- backend/readToDB.py
+++ backend/readToDB.py
@@ -12,7 +12,7 @@ data = df.head()
 # connect to db: pymysql
 # ORM: sqlalchemy
 
-engine = create_engine("mysql+pymysql://root:177177ooo@localhost:3306/isynet",encoding='utf-8')
+engine = create_engine("mysql+pymysql://root:password@localhost:3306/isynet",encoding='utf-8')
 
 df.to_sql("export",con=engine,if_exists='replace',index=False)
 
